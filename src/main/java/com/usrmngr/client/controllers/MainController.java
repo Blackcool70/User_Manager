1e2ef8045fcaf27a07443015f19bd060bf3ae2a1
Jecsan Blanco <jecsanb@outlook.com>
2019-03-17 17:50:41 -0500

    Added automatic textfield mapping between user object and fxml fields

--- src/main/java/com/usrmngr/client/controllers/MainController.java
+++ src/main/java/com/usrmngr/client/controllers/MainController.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
@@ -23,40 +24,54 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    // private final String DATA_PATH = "C:\\Users\\jecsa\\IdeaProjects\\User_Manager\\src\\main\\resources\\com\\usrmngr\\client\\samples\\data.json";
-    private final String DATA_PATH = "src/main/resources/samples/data.json";
+    // private final String DATA_PATH = "C:\\Users\\jecsa\\IdeaProjects\\User_Manager\\src\\main\\resources\\com\\usrmngr\\client\\samples\\MOCK_DATA.json";
+    private final String DATA_PATH = "src/main/resources/samples/MOCK_DATA.json";
     private JSONArray data;
 
+    private User selectedUser;
+
     @FXML
-    public VBox controlsVBox;
+    public VBox controlAreaPane;
     @FXML
     public GridPane userAreaPane, infoAreaPane, passwordAreaPane, licenseAreaPane, saveAreaPane;
     @FXML
     public TitledPane infoDropDown, passwordDropDown, licenseDropDown;
 
-    private User currentUser;
-    private FXNodeContainer controlArea, userArea, infoArea, licenseArea, passwordArea, saveArea;
-    public ListView<User> userList;
-    public TextField displayNameField, firstNameField, lastNameField, emailField, userPhoneField;
-    public Label usrID;
+    @FXML
     public Label userCount;
+    @FXML
+    public ListView<User> userList;
+    @FXML
+    public Label id;
+    @FXML
+    public TextField display_name, first_name, last_name, email_address, user_phone, middle_initial;
+    @FXML
+    public TextField job_title, department_name, office_name, manager_name, office_number, user_number;
+    @FXML
+    public PasswordField password_entry, password_confirm_entry;
+    private FXNodeContainer allNodes;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        controlArea = new FXNodeContainer(controlsVBox, false);
-        userArea = new FXNodeContainer(userAreaPane, true);
-        infoArea = new FXNodeContainer(infoAreaPane, true);
-        licenseArea = new FXNodeContainer(licenseAreaPane, true);
-        passwordArea = new FXNodeContainer(passwordAreaPane, true);
-        saveArea = new FXNodeContainer(saveAreaPane, false);
+
+        allNodes = new FXNodeContainer();
+        allNodes.addNodesFromParent(userAreaPane);
+        allNodes.addNodesFromParent(infoAreaPane);
+        allNodes.addNodesFromParent(passwordAreaPane);
+        allNodes.addNodesFromParent(licenseAreaPane);
+        allNodes.addNodesFromParent(saveAreaPane);
+
+        disableAllAreas(true);
         allAreasExpanded(false);
+
+        controlAreaPane.setDisable(false);
 
         //action for when a user gets double clicked on the list
         userList.setOnMouseClicked(event -> {
             if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2
             ) {
-                currentUser = userList.getSelectionModel().getSelectedItem();
-                loadUser(currentUser);
+                selectedUser = userList.getSelectionModel().getSelectedItem();
+                loadUser(selectedUser);
             }
         });
 
@@ -64,27 +79,19 @@ public class MainController implements Initializable {
     }
 
     private void loadUser(@NotNull User selectedUser) {
-        try {
-            JSONObject user = data.getJSONObject(Integer.parseInt(selectedUser.getId()) - 1);
-            displayNameField.setText(selectedUser.toString());
-            firstNameField.setText(user.getString("first_name"));
-            lastNameField.setText(user.getString("last_name"));
-            emailField.setText(user.getString("email"));
-            usrID.setText(selectedUser.getId());
-            userPhoneField.setText(user.getString("phone"));
-        } catch (JSONException e) {
-            displayError("Unable to load user, try again.");
+        String[] userAttributes = selectedUser.getAttributes();
+        for (String attribute : userAttributes) {
+            setTextOnTextField(attribute, selectedUser.getAttribute(attribute));
         }
-
     }
 
     private void loadUserList() {
         ObservableList<User> displayableUsers = FXCollections.observableArrayList();
         try {
             data = new JSONArray(DataManager.readFile(DATA_PATH));
-            User user;
             for (int i = 0; i < data.length(); i++) {
-                user = new User(data.getJSONObject(i));
+                JSONObject jsonObject = data.getJSONObject(i);
+                User user = new User(jsonObject);
                 displayableUsers.add(user);
             }
             userCount.setText(String.format("Users: %d", data.length()));
@@ -118,12 +125,12 @@ public class MainController implements Initializable {
     }
 
     private void disableAllAreas(boolean disable) {
-        controlArea.setDisable(disable);
-        userArea.setDisable(disable);
-        infoArea.setDisable(disable);
-        licenseArea.setDisable(disable);
-        passwordArea.setDisable(disable);
-        saveArea.setDisable(disable);
+        controlAreaPane.setDisable(disable);
+        userAreaPane.setDisable(disable);
+        infoAreaPane.setDisable(disable);
+        licenseAreaPane.setDisable(disable);
+        passwordAreaPane.setDisable(disable);
+        saveAreaPane.setDisable(disable);
     }
 
     private void loadSampleData() {
@@ -139,18 +146,18 @@ public class MainController implements Initializable {
 
     @FXML
     public void editButtonClicked() {
-        if(currentUser != null) {
-            controlArea.setDisable(true);
-            userArea.setDisable(false);
-            infoArea.setDisable(false);
-            licenseArea.setDisable(false);
-            saveArea.setDisable(false);
+        if (selectedUser != null) {
+            controlAreaPane.setDisable(true);
+            userAreaPane.setDisable(false);
+            infoAreaPane.setDisable(false);
+            licenseAreaPane.setDisable(false);
+            saveAreaPane.setDisable(false);
         }
     }
 
     @FXML
     public void addButtonClicked() {
-        controlArea.setDisable(true);
+        controlAreaPane.setDisable(true);
 //            clearFields();
         disableAllAreas(false);
         allAreasExpanded(true);
@@ -159,7 +166,7 @@ public class MainController implements Initializable {
     @FXML
     public void cancelButtonClicked() {
         disableAllAreas(true);
-        controlArea.setDisable(false);
+        controlAreaPane.setDisable(false);
         allAreasExpanded(false);
 //            clearFields();
     }
@@ -168,20 +175,20 @@ public class MainController implements Initializable {
     public void saveButtonClicked() {
         disableAllAreas(true);
         allAreasExpanded(false);
-        controlArea.setDisable(false);
+        controlAreaPane.setDisable(false);
     }
 
     @FXML
     public void passwordResetButtonClicked() {
         passwordDropDown.setExpanded(true);
-        controlArea.setDisable(true);
-        passwordArea.setDisable(false);
-        saveArea.setDisable(false);
+        controlAreaPane.setDisable(true);
+        passwordAreaPane.setDisable(false);
+        saveAreaPane.setDisable(false);
     }
 
     @FXML
     public void deleteButtonClicked() {
-        if (currentUser != null) {
+        if (selectedUser != null) {
             if (requestConfirmation("User will be deleted.")) {
                 System.out.println("User deleted!");
             }
@@ -194,8 +201,14 @@ public class MainController implements Initializable {
      * @param fieldName id of the field
      * @param text      the text to set on the field
      */
-    private void setTextOnField(String fieldName, String text) {
+    private void setTextOnTextField(String fieldName, String text) {
+        Node node = getNode(fieldName);
+        if (node instanceof TextField)
+            ((TextField) node).setText(text);
+    }
 
+    private Node getNode(String fieldName) {
+        return allNodes.getChild(fieldName);
     }
 
 }
