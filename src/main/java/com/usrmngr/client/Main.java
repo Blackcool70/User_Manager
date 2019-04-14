a9bc54744faa75822882c276b2afe68b2f02527f
Jecsan Blanco <jecsanb@outlook.com>
2019-04-13 20:10:53 -0500

    Created basic Config controller with its view and added AD config page to the view.

--- src/main/java/com/usrmngr/client/Main.java
+++ src/main/java/com/usrmngr/client/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     @Override
     //TODO: 2019-03-13  figure out the proper way to resolve paths
     public void start(Stage window) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ConfigView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ConfigView.fxml"));
         window.setTitle("User Manager: DEMO");
         window.setScene(new Scene(root));
         window.setResizable(false);
