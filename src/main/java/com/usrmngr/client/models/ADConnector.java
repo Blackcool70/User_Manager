7efd501fb258a9a3b40bd6ab954126b8b3e333e8
Jecsan Blanco <jecsanb@outlook.com>
2019-04-17 03:39:28 -0500

    Reformatted code and simplified class Able to search for any user and any attribute.

--- src/main/java/com/usrmngr/client/models/ADConnector.java
+++ src/main/java/com/usrmngr/client/models/ADConnector.java
@@ -7,128 +7,134 @@ import org.json.JSONObject;
 import java.util.Collection;
 
 public class ADConnector {
-    private LDAPConnection ldapConnection;
-    private String resultCode,errorMessageFromServer;
+    private final String USER_SEARCH_FILTER = "(&(objectClass=User)(anr=%s))";
+    private LDAPConnection connection;
+    private String resultCode, errorMessageFromServer;
+    private String server, bindDN, password;
+    private int port;
+    private String baseDN;
 
-    public ADConnector() {
-        ldapConnection = new LDAPConnection();
+    private ADConnector() {
+        setConfigs("", 289, "", "", "");
+        connection = new LDAPConnection();
     }
 
-    public ADConnector(String host, int port, String ldapSearchPath, String userName, String password) {
-        ldapConnection = new LDAPConnection();
-        connect(host, port, ldapSearchPath, userName, password);
-    }
-
-    public ADConnector(String host, int port) {
-        ldapConnection = new LDAPConnection();
-        connect(host, port);
-    }
-
-    /**
-     * Connects to an ldap AD server.
-     *
-     * @param host ldap enabled server name
-     * @param port ldap enabled server port
-     * @return true if the connection was successful otherwise false
-     */
-    private boolean connect(String host, int port) {
-        try {
-            ldapConnection.connect(host, port);
-        } catch (LDAPException e) {
-            resultCode = e.getResultCode().toString();
-            errorMessageFromServer = e.getDiagnosticMessage();
-        }
-        return isConnected();
-    }
-
-    private void connect(String host, int port, String ldapSearchPath, String userName, String password) {
-        connect(host, port);
-        String bindDN = String.format("CN=%s,%s", userName, ldapSearchPath);
-        bind(bindDN, password);
-    }
-
-    public boolean isConnected() {
-        return ldapConnection != null && ldapConnection.isConnected();
-    }
-
-    public void closeConnection() {
-        ldapConnection.close();
-    }
-
-    /**
-     * Returns a json object of the requested cn and its attributes.
-     * @param baseDN base search
-     * @param cn cn entry to search for
-     * @param attributes what attributes to return
-     * @return a json object of the the requested cn and its attributes.
-     */
-    private JSONObject getADUser(String baseDN,String cn, String ... attributes) {
-        if (!isConnected()) return null;
-        SearchResult searchResult = null;
-        StringBuilder filterSB = new StringBuilder(String.format("(&(objectCategory=person)(objectClass=user)(cn=%s))",cn));
-        try {
-            Filter  filter = Filter.create(filterSB.toString());
-            searchResult = ldapConnection.search(baseDN, SearchScope.SUB,filter.toString(),attributes);
-        } catch (LDAPException e) {
-            resultCode = e.getResultCode().toString();
-            errorMessageFromServer = e.getDiagnosticMessage();
-        }
-        return toJSON(searchResult);
-    }
-
-    private void bind(String bindDN, String password) {
-        if (this.isConnected()) {
-            try {
-                this.ldapConnection.bind(bindDN, password);
-            } catch (LDAPException e) {
-                resultCode = e.getResultCode().toString();
-            }
-        }
-    }
-
-    private JSONObject toJSON(SearchResult sr) {
-        if(sr == null) return  null;
-        JSONObject object = new JSONObject();
-        for(SearchResultEntry se : sr.getSearchEntries()){
-             Collection<Attribute> attributes =se.getAttributes();
-             for( Attribute a : attributes){
-                 try {
-                     object.put(a.getName(),a.getValue());
-                 } catch (JSONException e) {
-                     e.printStackTrace();
-                 }
-
-             }
-        }
-        return  object;
+    public ADConnector(String server, int port, String baseDN, String bindDN, String password) {
+        setConfigs(server, port, baseDN, bindDN, password);
+        connection = new LDAPConnection();
     }
 
     public static void main(String[] args) {
         ADConnector adConnector = new ADConnector();
-        adConnector.connect("192.168.1.2", 389);
-        if (adConnector.isConnected()) {
-            System.out.println("connected");
-            adConnector.bind("CN=Administrator,OU=Users,OU=Company,DC=lab,DC=net", "xxxxxxxx");
-            JSONObject object = adConnector.getADUser("dc=lab,dc=net", "Administratr","distinguishedName");
-            if(object == null){
-                System.out.println(adConnector.errorMessageFromServer);
-            }else {
-                System.out.println(object);
-                System.out.println(adConnector.getErrorMessageFromServer());
-            }
-
+        adConnector.setConfigs("192.168.1.2", 389, "DC=lab,DC=net",
+                "cn=Administrator,ou=Users,ou=Company,dc=lab,dc=net", "xxxxxxxx");
+        if (adConnector.connect()) {
+            System.out.println("Connected");
         } else {
-            System.out.println("Connection failed");
-            System.out.println(adConnector.getResultCode());
+            System.out.printf("Unable to connect %s\n", adConnector.getResultCode());
         }
+        JSONObject jsonObject = adConnector.getADUser("Administrator", "mail");
+        System.out.println(jsonObject);
+        System.out.println(adConnector.getResultCode());
 
+    }
+
+    private void setConfigs(String server, int port, String baseDN, String bindDN, String password) {
+        if (isConnected()) disconnect();
+        this.server = server;
+        this.port = port;
+        this.bindDN = bindDN;
+        this.baseDN = baseDN;
+        this.password = password;
+    }
+
+    private boolean connect() {
+        try {
+            connection.connect(server, port);
+        } catch (LDAPException e) {
+            resultCode = e.getResultCode().toString();
+            errorMessageFromServer = e.getDiagnosticMessage();
+        }
+        return isConnected() && bind(bindDN, password);
+    }
+
+    private boolean bind(String bindDN, String password) {
+        if (!this.isConnected()) return false;
+        boolean result = true;
+        try {
+            this.connection.bind(bindDN, password);
+        } catch (LDAPException e) {
+            resultCode = e.getResultCode().toString();
+            result = false;
+        }
+        return result;
+    }
+
+    public boolean isConnected() {
+        return connection != null && connection.isConnected();
+    }
+
+    private void disconnect() {
+        connection.close();
+    }
+
+    /**
+     * Returns a json object of the requested cn and its attributes.
+     *
+     * @param attributes what attributes to return
+     * @return a json object of the the requested cn and its attributes.
+     */
+    private JSONObject getADUser(String userName, String... attributes) {
+        SearchResult searchResult = null;
+        StringBuilder filterSB = new StringBuilder(String.format(USER_SEARCH_FILTER, userName));
+        boolean result = true;
+        try {
+            Filter filter = Filter.create(filterSB.toString());
+            System.out.println("Filter:" + filter.toString());
+            System.out.println("Attributes:" + attributes[0]);
+            SearchRequest searchRequest =
+                    new SearchRequest(baseDN, SearchScope.SUB, filter,
+                            attributes);
+            searchResult = connection.search(searchRequest);
+        } catch (LDAPException e) {
+            resultCode = e.getResultCode().toString();
+            errorMessageFromServer = e.getDiagnosticMessage();
+            result = false;
+        }
+        return result ? toJSON(searchResult) : new JSONObject();
+    }
+
+    private JSONObject toJSON(SearchResult sr) {
+        JSONObject object = new JSONObject();
+        for (SearchResultEntry se : sr.getSearchEntries()) {
+            Collection<Attribute> attributes = se.getAttributes();
+            for (Attribute a : attributes) {
+                try {
+                    object.put(a.getName(), a.getValue());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        return object;
     }
 
     public String getResultCode() {
-        return  this.resultCode;
+        return this.resultCode == null ? "" : this.resultCode;
     }
-    private String getErrorMessageFromServer(){
-        return  this.errorMessageFromServer;
+
+    private String getErrorMessageFromServer() {
+        return this.errorMessageFromServer == null ? "" : this.resultCode;
+    }
+
+    @Override
+    public String toString() {
+        return "server=" + this.server + "\n" +
+                "port=" + this.port + "\n" +
+                "baseDN=" + this.baseDN + "\n" +
+                "bindDN=" + this.bindDN + "\n" +
+                "password=" + this.password + "\n";
     }
 }
 
