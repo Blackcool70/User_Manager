2c6c8730388061e163292f72e9cb2ea30fc61af1
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2019-05-01 14:01:13 -0500

    Changed names of fields to be used as the names for the properties to be saved. Updated field names.

--- src/main/java/com/usrmngr/client/controllers/ConfigADController.java
+++ src/main/java/com/usrmngr/client/controllers/ConfigADController.java
@@ -1,8 +1,7 @@
 package com.usrmngr.client.controllers;
 
-import com.usrmngr.client.Main;
-import com.usrmngr.client.models.ADConnector;
 import com.usrmngr.client.models.FXNodeContainer;
+import com.usrmngr.client.util.DataManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -18,56 +18,29 @@ public class ConfigADController implements Initializable {
     @FXML
     public Pane root;
     @FXML
-    public TextField authDomain;
-    @FXML
-    public TextField ldapPath;
-    @FXML
-    public TextField hostName;
-    @FXML
-    public TextField userName;
-    @FXML
-    public TextField port;
-    @FXML
     public Button testButton;
     @FXML
     public Label message;
 
     private Properties properties;
-    private FXNodeContainer fxNodeContainer;
-
+    private FXNodeContainer allNodes;
+    private ArrayList<TextField> textFields;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        fxNodeContainer = new FXNodeContainer();
-        fxNodeContainer.addItem(root);
-        properties = Main.getProperties();
-        testButton.setOnMouseClicked(
-                event -> testButtonClicked()
-        );
+        allNodes = new FXNodeContainer();
+        allNodes.addItem(root);
+        textFields = allNodes.getTextFields();
 
     }
+    private void load(){
+        properties = DataManager.getProperties();
+        textFields.forEach(textField ->
+                textField.setText((String)properties.getOrDefault(textField.getId(),"")));
 
-    private void testButtonClicked() {
-
-        ADConnector adConnector = new ADConnector();
-        if(!properties.isEmpty()){
-            String server = properties.getProperty("hostName");
-            int port = Integer.parseInt(properties.getProperty("port"));
-            String baseDN = properties.getProperty("ldapPath");
-            String un = properties.getProperty("userName");
-            String bindDN = un.concat(",").concat(baseDN);
-            String password = properties.getProperty("password");
-            adConnector.setConfigs(server, port, baseDN, bindDN, password);
-            adConnector.connect();
-        }
-
-        String msg;
-        if(adConnector.isConnected()){
-            msg = "Connection Successful";
-        }else{
-            msg = "Connection Failed: ".concat(adConnector.getResultCode());
-        }
-        message.setText(msg.concat(adConnector.getErrorMessageFromServer()));
-        adConnector.disconnect();
     }
+    private   void save(){
+        textFields.forEach(textField ->
+                properties.setProperty(textField.getId(),textField.getText()));
 
+    }
 }
