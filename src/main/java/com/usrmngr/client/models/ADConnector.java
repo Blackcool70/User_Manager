7a940146cf77e48a5147bb0adcaff3435ceaaddb
Jecsan Blanco <jecsanb@outlook.com>
2019-05-02 02:30:53 -0500

    Updated to support settings via properties. Re implemented searchUser and SearchAll along as result to json and json arrays.

--- src/main/java/com/usrmngr/client/models/ADConnector.java
+++ src/main/java/com/usrmngr/client/models/ADConnector.java
@@ -1,18 +1,24 @@
 package com.usrmngr.client.models;
 
 import com.unboundid.ldap.sdk.*;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.Properties;
 
 public class ADConnector {
 
     private LDAPConnection connection;
-    private String resultCode, errorMessageFromServer;
-    private String server, bindCN, password;
+    private String server;
     private int port;
+    private String bindCN;
     private String baseDN;
+    private String password;
+    private String resultCode;
+    private String errorMessageFromServer;
 
     public ADConnector() {
         setConfigs("", 389, "", "", "");
@@ -24,18 +30,29 @@ public class ADConnector {
         connection = new LDAPConnection();
     }
 
+    public ADConnector(Properties properties) {
+        if (properties == null) return;
+        setConfigs((String) properties.getOrDefault("server", ""),
+                Integer.parseInt((String) properties.getOrDefault("port", "389")),
+                (String) properties.getOrDefault("baseDN", ""),
+                (String) properties.getOrDefault("bindDN", ""),
+                (String) properties.getOrDefault("password", ""));
+    }
+
     public static void main(String[] args) {
         ADConnector adConnector = new ADConnector();
         adConnector.setConfigs("192.168.1.2", 389, "DC=lab,DC=net",
-                "cn=Administrator,ou=Users,ou=Company,dc=lab,dc=net", "xxxxxxxx");
+                "cn=Administrator,ou=Users,ou=Company,dc=lab,dc=net", "J3cs4nb!");
         if (adConnector.connect()) {
             System.out.println("Connected");
         } else {
             System.out.printf("Unable to connect %s\n", adConnector.getResultCode());
         }
-        JSONObject jsonObject = adConnector.getADUser("Administrator", "mail");
-        System.out.println(jsonObject);
-        System.out.println(adConnector.getResultCode());
+//        JSONObject jsonObject = adConnector.getADUser("Administrator", "mail");
+//        System.out.println(jsonObject);
+//        System.out.println(adConnector.getResultCode());
+        JSONArray jsonArray = adConnector.getAllADUsers("displayName");
+        System.out.println(jsonArray);
 
     }
 
@@ -87,42 +104,29 @@ public class ADConnector {
      * @return a json object of the the requested cn and its attributes.
      */
     private JSONObject getADUser(String userName, String... attributes) {
-        SearchResult searchResult = null;
-        StringBuilder filterSB = new StringBuilder(String.format(LDAPFilters.USER_SEARCH_FILTER, userName));
-        boolean result = true;
-        try {
-            Filter filter = Filter.create(filterSB.toString());
-//            System.out.println("Filter:" + filter.toString());
-//            System.out.println("Attributes:" + attributes[0]);
-            SearchRequest searchRequest =
-                    new SearchRequest(baseDN, SearchScope.SUB, filter,
-                            attributes);
-            searchResult = connection.search(searchRequest);
-        } catch (LDAPException e) {
-            resultCode = e.getResultCode().toString();
-            errorMessageFromServer = e.getDiagnosticMessage();
-            result = false;
+        JSONObject jsonObject = new JSONObject();
+        Optional<SearchResult> optionalSearchResult = searchAD(String.format(LDAPFilters.USER_SEARCH_FILTER, userName), attributes);
+        if (optionalSearchResult.isPresent()) {
+            jsonObject = toJSON(optionalSearchResult.get().getSearchEntries().get(0));
         }
-        return result ? toJSON(searchResult) : new JSONObject();
+        return jsonObject;
     }
 
-    private JSONObject toJSON(SearchResult sr) {
+    private JSONObject toJSON(SearchResultEntry se) {
         JSONObject object = new JSONObject();
-        for (SearchResultEntry se : sr.getSearchEntries()) {
+        try {
+            object.put("DN", se.getDN());
             Collection<Attribute> attributes = se.getAttributes();
             for (Attribute a : attributes) {
-                try {
-                    object.put(a.getName(), a.getValue());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
+                object.put(a.getName(), a.getValue());
             }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
         return object;
     }
 
-    public String getResultCode() {
+    private String getResultCode() {
         return this.resultCode == null ? "" : this.resultCode;
     }
 
@@ -139,8 +143,34 @@ public class ADConnector {
                 "password:" + this.password + "\n";
     }
 
+    public JSONArray getAllADUsers(String... attributes) {
+        JSONArray jsonArray = new JSONArray();
+        Optional<SearchResult> optional = searchAD(LDAPFilters.ALL_USER_SEARCH_FILTER, attributes);
+        optional.ifPresent(searchResult -> searchResult.getSearchEntries().forEach(
+                searchResultEntry -> jsonArray.put(toJSON(searchResultEntry))));
+        return jsonArray;
+    }
+
+    private Optional<SearchResult> searchAD(String query, String... attributes) {
+        Optional<SearchResult> result = Optional.empty();
+        try {
+            Filter filter = Filter.create(query);
+//            System.out.println("Filter:" + filter.toString());
+//            System.out.println("Attributes:" + attributes[0]);
+            SearchRequest searchRequest =
+                    new SearchRequest(baseDN, SearchScope.SUB, filter,
+                            attributes);
+            result = Optional.of(connection.search(searchRequest));
+        } catch (LDAPException e) {
+            resultCode = e.getResultCode().toString();
+            errorMessageFromServer = e.getDiagnosticMessage();
+        }
+        return result;
+    }
+
     private static class LDAPFilters {
         private static final String USER_SEARCH_FILTER = "(&(objectClass=User)(anr=%s))";
+        private static final String ALL_USER_SEARCH_FILTER = "(objectClass=User)";
     }
 }
 
