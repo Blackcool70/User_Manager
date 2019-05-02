d9ff186f670725200f216491175b564088c2cee6
Jecsan Blanco <jecsanb@outlook.com>
2019-05-02 18:26:38 -0500

    Added extra param to be able to customize details on alerts.

--- src/main/java/com/usrmngr/client/models/FXDialogs/DialogManager.java
+++ src/main/java/com/usrmngr/client/models/FXDialogs/DialogManager.java
@@ -7,27 +7,28 @@ import javafx.scene.control.ButtonType;
 import java.util.Optional;
 
 public class DialogManager {
-    public static boolean requestConfirmation(String message) {
+    public static boolean requestConfirmation(String message,String details) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirm");
+        alert.setTitle("Confirmation requested");
         alert.setHeaderText(message);
-        alert.setContentText("Are you sure?");
+        alert.setContentText(details);
         Optional<ButtonType> result = alert.showAndWait();
-        return result.get() == ButtonType.OK;
+        return result.filter(buttonType -> buttonType == ButtonType.OK).isPresent();
 
     }
 
-    public static void showError(String message, boolean abort){
-        showError(message);
+    public static void showError(String message,String details, boolean abort){
+        showError(message,details);
         if(abort) {
             Platform.exit();
             System.exit(1);
         }
     }
-     private static void showError(String message) {
+     private static void showError(String message,String details) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText(message);
+        alert.setContentText(details);
         alert.showAndWait();
     }
 
