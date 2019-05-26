0a25da2c40d0115267ddeae52973ea2518186a65
Jecsan Blanco <jecsanb@outlook.com>
2019-05-26 01:22:26 -0500

    added log4j2 config

--- src/main/java/com/usrmngr/server/core/model/Constants.java
+++ src/main/java/com/usrmngr/server/core/model/Constants.java
@@ -3,4 +3,5 @@ package com.usrmngr.server.core.model;
 public final class Constants {
    public static final String CONFIG_FILE = "configs.txt";
    public static final String APP_NAME = "UserManager";
+
 }
