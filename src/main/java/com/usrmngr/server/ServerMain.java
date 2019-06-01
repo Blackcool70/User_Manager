e36625970257fd8175d4229ae11c9aea063c314e
Jecsan Blanco <jecsanb@outlook.com>
2019-06-01 11:38:57 -0500

    server will now gracefully shutdown if server window is closed.

--- src/main/java/com/usrmngr/server/ServerMain.java
+++ src/main/java/com/usrmngr/server/ServerMain.java
@@ -1,5 +1,7 @@
 package com.usrmngr.server;
 
+import com.usrmngr.server.core.model.Server;
+import com.usrmngr.server.ui.controllers.ServerMainViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,17 +9,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ServerMain extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage window) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/server/fxml/ServerMainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/server/fxml/ServerMainView.fxml"));
+        Parent root = loader.load();
         Scene scene = new Scene(root);
         window.setScene(scene);
-        window.show();
         window.setTitle("Server Manager Client");
+        ServerMainViewController controller = loader.getController();
+
+        window.setOnCloseRequest(e -> controller.shutdown());
+        window.show();
 
     }
 }
