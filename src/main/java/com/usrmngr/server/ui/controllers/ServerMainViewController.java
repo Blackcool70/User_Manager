654edc4ce8187fa979249c8a51f3f79261b5c830
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2019-06-01 20:29:39 -0500

    added about window

--- src/main/java/com/usrmngr/server/ui/controllers/ServerMainViewController.java
+++ src/main/java/com/usrmngr/server/ui/controllers/ServerMainViewController.java
@@ -3,13 +3,23 @@ package com.usrmngr.server.ui.controllers;
 import com.usrmngr.server.core.model.Server;
 import com.usrmngr.util.Alert.AlertMaker;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 
 public class ServerMainViewController implements Initializable {
@@ -73,7 +83,24 @@ public class ServerMainViewController implements Initializable {
     }
 
     private void showAboutWindow() {
-        System.out.println("Opening about Window.");
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/server/fxml/About.fxml"));
+            Scene aboutScene = new Scene(root);
+            Stage configWindow = new Stage();
+            // prevents the parent window from being modified before configs are closed.
+            configWindow.initModality(Modality.WINDOW_MODAL);
+            configWindow.initOwner(Stage.getWindows().filtered(Window::isShowing).get(0));
+            configWindow.setTitle("About");
+            configWindow.setScene(aboutScene);
+            configWindow.setMaxHeight(426);
+            configWindow.setWidth(305);
+            configWindow.setResizable(false);
+            configWindow.showAndWait();
+        } catch (IOException e) {
+            AlertMaker.showErrorMessage(e, "Unable to load", "Failed to load about.");
+        }
+
+
     }
 
     private void initServer() {
