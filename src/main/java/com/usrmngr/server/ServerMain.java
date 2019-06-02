42bf319da3bc46dd4ee69ebcfd46c29a3f42b397
Jecsan Blanco <jecsanb@outlook.com>
2019-06-02 15:20:36 -0500

    Removed unused code and fixed typos.

--- src/main/java/com/usrmngr/server/ServerMain.java
+++ src/main/java/com/usrmngr/server/ServerMain.java
@@ -6,16 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class ServerMain extends Application {
-    private static final Logger LOGGER = LogManager.getLogger(ServerMain.class.getName());
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -25,20 +17,11 @@ public class ServerMain extends Application {
         Parent root = loader.load();
         Scene scene = new Scene(root);
         window.setScene(scene);
-        window.setTitle("Server Manager Client");
+        window.setTitle("User Manager Server");
         ServerMainViewController controller = loader.getController();
         window.setOnCloseRequest(e -> controller.shutdown());
         window.show();
 
     }
-    private void initLogging() {
-        //configure global app  logging
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        LOGGER.log(Level.INFO, "Application started on {}", dateFormat.format(new Date()));
 
-        Thread thread = new Thread(() ->
-                LOGGER.log(Level.INFO, "Application stopped on {}.", dateFormat.format(new Date())));
-        thread.setName("Server");
-        Runtime.getRuntime().addShutdownHook(thread);
-    }
 }
