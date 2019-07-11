c4ff04dcebf5e185ff036fe7adf8f53e37911e84
Jecsan Blanco <jecsanb@outlook.com>
2019-07-10 20:02:09 -0500

    Switched to quickserver composition instead of inheritance.

--- src/main/java/com/usrmngr/server/core/model/QuickServer/UMServer/UMServer.java
+++ src/main/java/com/usrmngr/server/core/model/QuickServer/UMServer/UMServer.java
@@ -1,29 +1,58 @@
 package com.usrmngr.server.core.model.QuickServer.UMServer;
-
+import com.usrmngr.client.core.UMClient;
+import javafx.scene.Parent;
+import org.json.JSONObject;
 import org.quickserver.net.AppException;
 import org.quickserver.net.server.QuickServer;
 
-public class UMServer extends QuickServer {
-    private static String VER = "1.0";
-    private static String cmdHandle = "com.usrmngr.server.core.model.QuickServer.UMServer.RequestCommandHandler";
+public class UMServer  {
+    private  String cmdHandle = "com.usrmngr.server.core.model.QuickServer.UMServer.RequestCommandHandler";
+    private  QuickServer server;
 
+    private  final static int PORT = 8011;
+    private  final static String SERVER_NAME = "UM Server";
+    private boolean running;
+
+    /**
+     * Core server that is manipulated by the GUI
+     */
     public UMServer() {
-        super(cmdHandle);
-        this.setPort(8125);
-        this.setName("UM Server Version " + VER);
-        this.getBasicConfig().getServerMode().setBlocking(true);
-        this.getBasicConfig().getAdvancedSettings().setDebugNonBlockingMode(true);
-        this.getBasicConfig().setCommunicationLogging(true);
+        server = new QuickServer(cmdHandle);
+        server.setPort(PORT);
+        server.setName(SERVER_NAME);
+        server.getBasicConfig().getServerMode().setBlocking(true);
+        server.getBasicConfig().getAdvancedSettings().setDebugNonBlockingMode(true);
+        server.getBasicConfig().setCommunicationLogging(true);
+    }
+
+    public void startServer(){
+        try {
+            server.startServer();
+            this.running = true;
+        } catch (AppException e) {
+            // what is best to do in this case?
+            e.printStackTrace();
+        }
+    }
+    public void stopServer(){
+        try {
+            server.stopServer();
+            running = false;
+        } catch (AppException e) {
+            // what is best to do in this case?
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         UMServer umServer = new UMServer();
-        try {
-            umServer.startServer();
-        } catch (AppException e) {
-            System.out.println("Error in server : " + e);
-            e.printStackTrace();
-        }
+        umServer.startServer();
+        System.out.println(umServer.isRunning());
+        umServer.stopServer();
+        System.out.println(umServer.isRunning());
+    }
 
+    public boolean isRunning() {
+        return running;
     }
 }
