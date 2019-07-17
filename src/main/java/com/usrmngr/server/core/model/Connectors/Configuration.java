31b28ee118922a12e7891538b332117be1094569
Jecsan Blanco <jecsanb@outlook.com>
2019-07-16 20:28:54 -0500

    corrected load and save functions to save config file.

--- src/main/java/com/usrmngr/server/core/model/Connectors/Configuration.java
+++ src/main/java/com/usrmngr/server/core/model/Connectors/Configuration.java
@@ -5,10 +5,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
+import static com.usrmngr.server.ServerMain.APP_NAME;
+
 class Configuration {
     private Properties values;
 
-   Configuration() {
+    Configuration() {
         values = new Properties();
     }
 
@@ -20,11 +22,13 @@ class Configuration {
         return values.getProperty(key);
     }
 
-    private void load(String path) throws IOException {
-        // First try loading from the current directory
-        File f = new File(path);
-        InputStream is = new FileInputStream(f);
-        values.load(is);
+    private void load(String fileName) throws IOException {
+        File f = new File(getUserDataDirectory().concat(fileName));
+        if(f.exists()) {
+            InputStream is = new FileInputStream(f);
+            values.load(is);
+        }
+
     }
 
     private String getDateTime() {
@@ -39,36 +43,57 @@ class Configuration {
         values.loadFromXML(is);
 
     }
-    private void saveToXML(String path) throws IOException {
-        File f = new File(path);
-        f.mkdir();
-        OutputStream out = new FileOutputStream(f);
-        values.storeToXML(out, getDateTime());
+
+    private void saveToXML(String fileName) throws IOException {
+        String dataPath = getUserDataDirectory().concat(fileName);
+        File configFile = new File(dataPath);
+        configFile.getParentFile().mkdir();
+        configFile.createNewFile();
+        values.storeToXML(new FileOutputStream(configFile), getDateTime());;
 
     }
-    private void save(String path) throws IOException {
-        File f = new File(path);
-        f.mkdir();
-        OutputStream out = new FileOutputStream(f);
-        values.store(out, getDateTime());
+
+    private static String getUserDataDirectory() {
+        return System.getProperty("user.home") + File.separator + ".".concat(APP_NAME.toLowerCase()) + File.separator;
     }
 
-    /**
-     * Returns string representation of the config
-     *
-     * @return string representation
-     */
-    @Override
-    public String toString() {
-        return values.toString();
+    public  void save(String fileName) throws IOException {
+        String dataPath = getUserDataDirectory().concat(fileName);
+        File configFile = new File(dataPath);
+        configFile.getParentFile().mkdir();
+        configFile.createNewFile();
+        values.store(new FileOutputStream(configFile),"");
+
     }
 
-    public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.put("taco", "test");
-        configuration.put("taco1", "test1");
-        configuration.put("taco12", "test12");
-        System.out.println(configuration);
+        /**
+         * Returns string representation of the config
+         *
+         * @return string representation
+         */
+        @Override
+        public String toString () {
+            return values.toString();
+        }
 
+        public static void main (String[]args){
+            Configuration configuration = new Configuration();
+            configuration.put("taco", "test");
+            configuration.put("taco1", "test1");
+            configuration.put("taco12", "test12");
+            try {
+                configuration.saveToXML("test.config.");
+                configuration.clear();
+                configuration.load("test.config");
+                System.out.println(configuration);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(configuration);
+
+        }
+
+    private void clear() {
+            values.clear();
     }
 }
