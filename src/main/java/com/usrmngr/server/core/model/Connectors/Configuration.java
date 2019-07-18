10c31ac75c665dd2721dbb8536f09d63a304e771
Jecsan Blanco <jecsanb@outlook.com>
2019-07-17 22:59:00 -0500

    Cleaned up config class and used inheritance instead of composition for LDAPConfig

--- src/main/java/com/usrmngr/server/core/model/Connectors/Configuration.java
+++ src/main/java/com/usrmngr/server/core/model/Connectors/Configuration.java
@@ -1,99 +1,88 @@
 package com.usrmngr.server.core.model.Connectors;
 
-import java.io.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 import static com.usrmngr.server.ServerMain.APP_NAME;
 
-class Configuration {
+/**
+ * A generic configuration class that can hold different types of values.
+ * The class is able to open files and write files to the desired paths.
+ */
+public class Configuration {
     private Properties values;
-
-    Configuration() {
+    public Configuration() {
         values = new Properties();
     }
 
-    void put(String key, String value) {
+    /**
+     * Deep Copy constructor
+     * @param conf another configuration
+     */
+    Configuration(Configuration conf) {
+        this.values = new Properties(conf.values);
+    }
+
+    /**
+     * Deep clone
+     * @param configuration
+     * @return deep clone of provided config
+     */
+    public Configuration clone(Configuration configuration) {
+        return new Configuration(configuration);
+    }
+    protected void put(String key, String value) {
         values.put(key, value);
     }
 
-    String get(String key) {
+     String get(String key) {
         return values.getProperty(key);
     }
 
-    private void load(String fileName) throws IOException {
-        File f = new File(getUserDataDirectory().concat(fileName));
-        if(f.exists()) {
-            InputStream is = new FileInputStream(f);
-            values.load(is);
-        }
-
+    /**
+     * Attempts to load the configuration file at the path specified.
+     * @param path - exact path of the configuration file.
+     * @throws IOException
+     */
+    public void load(String path) throws IOException {
+        File file = new File(path);
+        values.load(new FileInputStream(file));
     }
 
-    private String getDateTime() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        return LocalDateTime.now().toString();
-    }
-
-    private void loadFromXML(String path) throws IOException {
-        // First try loading from the current directory
-        File f = new File(path);
-        InputStream is = new FileInputStream(f);
-        values.loadFromXML(is);
-
-    }
-
-    private void saveToXML(String fileName) throws IOException {
-        String dataPath = getUserDataDirectory().concat(fileName);
-        File configFile = new File(dataPath);
-        configFile.getParentFile().mkdir();
+    /**
+     *  Attempts to save the configuration file at the path specified.
+     * @param path - exact path and of where to save the file.
+     * @throws IOException
+     */
+    public void save(String path) throws IOException {
+        File configFile = new File(path); // maybe  give an option to name the file and just save the file the path.
+        File parent = configFile.getParentFile();
+        if(parent != null) // create directory structure as needed
+            parent.mkdir();
         configFile.createNewFile();
-        values.storeToXML(new FileOutputStream(configFile), getDateTime());;
-
+        values.store(new FileOutputStream(configFile), "config");
     }
 
-    private static String getUserDataDirectory() {
-        return System.getProperty("user.home") + File.separator + ".".concat(APP_NAME.toLowerCase()) + File.separator;
+
+    /**
+     * Returns string representation of the config
+     *
+     * @return string representation
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        values.forEach((key, value) -> sb.append(key).append("=").append(value).append("\n"));
+        sb.deleteCharAt(sb.lastIndexOf("\n"));
+        return sb.toString();
     }
 
-    public  void save(String fileName) throws IOException {
-        String dataPath = getUserDataDirectory().concat(fileName);
-        File configFile = new File(dataPath);
-        configFile.getParentFile().mkdir();
-        configFile.createNewFile();
-        values.store(new FileOutputStream(configFile),"");
-
+    public void clear() {
+        values.clear();
     }
-
-        /**
-         * Returns string representation of the config
-         *
-         * @return string representation
-         */
-        @Override
-        public String toString () {
-            return values.toString();
-        }
-
-        public static void main (String[]args){
-            Configuration configuration = new Configuration();
-            configuration.put("taco", "test");
-            configuration.put("taco1", "test1");
-            configuration.put("taco12", "test12");
-            try {
-                configuration.saveToXML("test.config.");
-                configuration.clear();
-                configuration.load("test.config");
-                System.out.println(configuration);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println(configuration);
-
-        }
-
-    private void clear() {
-            values.clear();
+    public static void main(String[] args) {
     }
 }
