ac5baecededcc1a81e2d4dde4b1d59faab43db5d
Jecsan Blanco <jecsanb@outlook.com>
2019-07-17 23:04:25 -0500

    updated, added comments.

--- src/main/java/com/usrmngr/server/core/model/Connectors/LDAPConnector.java
+++ src/main/java/com/usrmngr/server/core/model/Connectors/LDAPConnector.java
@@ -7,42 +7,62 @@ import org.json.JSONObject;
 
 import java.util.Collection;
 
+/**
+ * Encapsulates  an ldap connection
+ */
 public class LDAPConnector {
     private LDAPConnection connection;
     private BindResult bindResult;
-    private LDAPConnectorConfig config;
+    private LDAPConfig config;
 
     LDAPConnector() {
-        config = new LDAPConnectorConfig();
         connection = new LDAPConnection();
     }
 
-    public LDAPConnector(LDAPConnectorConfig config) {
+    public LDAPConnector(LDAPConfig config) {
+        this.config = new LDAPConfig(config);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Returns the current config.
+     * @return the current config.
+     */
+    public LDAPConfig getConfig() {
+        return this.config;
+    }
+
+    /**
+     * Sets the config file to be used by the connector.
+     * @param config
+     */
+    public void setConfig(LDAPConfig config) {
         this.config = config;
     }
 
-    public void setHostName(String host) {
-        this.config.setHostName(host);
+
+    public String getBaseDN() {
+        return this.config.getBaseDN();
+    }
+
+    public void setBaseDN(String dn) {
+        this.config.setBaseDN(dn);
+    }
+
+    public int getPort() {
+        return this.config.getPort();
     }
 
     public void setPort(int port) {
         this.config.setPort(port);
     }
 
-    public void setBaseDN(String dn){
-        this.config.setBaseDN(dn);
-    }
-    public String getBaseDN(){
-        return this.config.getBaseDN();
-    }
-    public int getPort() {
-        return this.config.getPort();
-    }
-
     public String getHost() {
         return this.config.getHostName();
     }
-
 
     public void authenticate(String userName, String password) {
         try {
@@ -77,14 +97,16 @@ public class LDAPConnector {
         }
         return object;
     }
-    private JSONArray resultsToJSONArray(SearchResult result){
+
+    private JSONArray resultsToJSONArray(SearchResult result) {
         JSONArray jsonArray = new JSONArray();
-        if(result == null) return jsonArray;
+        if (result == null) return jsonArray;
         result.getSearchEntries().forEach(
                 searchResultEntry -> jsonArray.put(toJSON(searchResultEntry)));
-        return  jsonArray;
+        return jsonArray;
     }
-    private JSONArray search(String query,String ...attributes) {
+
+    protected JSONArray search(String query, String... attributes) {
         SearchResult result = null;
         try {
             Filter filter = Filter.create(query);
@@ -95,30 +117,14 @@ public class LDAPConnector {
             result = connection.search(searchRequest);
         } catch (LDAPException ignored) {
         }
-        return  resultsToJSONArray(result);
+        return resultsToJSONArray(result);
     }
 
     public boolean isAuthenticated() {
         return bindResult != null && bindResult.getResultCode() == ResultCode.SUCCESS;
     }
 
-    public static void main(String[] args) {
-        LDAPConnectorConfig lc = new LDAPConnectorConfig();
-        lc.setBaseDN("DC=lab,DC=net");
-        lc.setHostName("192.168.1.2");
-        lc.setPort(389);
-        LDAPConnector ld = new LDAPConnector(lc);
-        ld.connect();
-        System.out.println("Connected: " + ld.isConnected());
-        ld.authenticate("CN=Administrator,OU=Users,OU=Company,DC=lab,DC=net", "J3cs4nb!");
-        System.out.println("Auth:" + ld.isAuthenticated());
-        JSONArray se = ld.search( "(objectClass=User)");
-        System.out.println(se.toString());
-
-
-    }
-
-    private void disconnect() {
+    void disconnect() {
         if (isConnected()) {
             connection.close();
         }
