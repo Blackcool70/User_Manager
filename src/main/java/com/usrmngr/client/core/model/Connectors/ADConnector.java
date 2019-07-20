1b857b018a81c1580b622f7364cb5675266f1f94
Jecsan Blanco <jecsanb@outlook.com>
2019-07-20 13:20:45 -0500

    Added alert when a user is unable to be loaded.

--- src/main/java/com/usrmngr/client/core/model/Connectors/ADConnector.java
+++ src/main/java/com/usrmngr/client/core/model/Connectors/ADConnector.java
@@ -1,7 +1,9 @@
 package com.usrmngr.client.core.model.Connectors;
 
+import com.usrmngr.util.Alert.AlertMaker;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 
 /**
  * Encapsulates actions that can be preformed in active directory services.
@@ -10,44 +12,43 @@ public class ADConnector extends LDAPConnector {
     public ADConnector() {
         super();
     }
-    public ADConnector(LDAPConfig config) {
+    public ADConnector(LDAPConfig config){
         super();
         this.setConfig(config);
     }
 
-    JSONObject getADUser(String cn, String... attributes) {
-        if (!isConnected()) return null; // or should I through exception?
+    public JSONObject getADUser(String cn, String... attributes) {
         JSONArray results = search(String.format(ADLDAPFilters.USER_SEARCH_FILTER, cn), attributes);
+        if(results.isEmpty()) {
+            AlertMaker.showSimpleAlert("Alert", "Unable to get user. Try again.");
+            //may want to refresh user list when this fails
+        }
         return results.getJSONObject(0);
     }
-    public JSONArray getAllADUsers(){
-        if (!isConnected()) return null; // or should I through exception?
-        return search(ADLDAPFilters.ALL_USER_SEARCH_FILTER,"displayName");
+    public void setConfig(LDAPConfig config){
+        super.setConfig(config);
+    }
+    public JSONArray getAllADUsers(String ... attributes){
+        return search(ADLDAPFilters.ALL_USER_SEARCH_FILTER,attributes);
     }
 
     public static void main(String[] args) {
         ADConnector adConnector = new ADConnector();
         LDAPConfig config = new LDAPConfig();
-        config.setBaseDN("dc=lab,dc=net");
         config.setHostName("192.168.1.2");
+        config.setBaseDN("dc=lab,dc=net");
         config.setPort(389);
         adConnector.setConfig(config);
         adConnector.connect();
         System.out.println(adConnector.isConnected());
         adConnector.authenticate("cn=Administrator,ou=Users,ou=Company,dc=lab,dc=net", "J3cs4nb!");
         System.out.println(adConnector.isAuthenticated());
-        System.out.println(adConnector.getADUser("Administrator", "displayName"));
-        JSONArray users = adConnector.getAllADUsers();
-        for (int i=0; i < users.length(); i++) {
-            JSONObject user = users.getJSONObject(i);
-            if(user.has("displayName")) System.out.println(user.get("displayName"));
-
-        }
+        System.out.println(adConnector.getADUser("Administrator","cn","dn","displayName"));
 
     }
 
     private static class ADLDAPFilters {
-        private static final String USER_SEARCH_FILTER = "(&(objectClass=User)(anr=%s))";
+        private static final String USER_SEARCH_FILTER = "(cn=%s)";
         private static final String ALL_USER_SEARCH_FILTER = "(objectClass=User)";
     }
 }
