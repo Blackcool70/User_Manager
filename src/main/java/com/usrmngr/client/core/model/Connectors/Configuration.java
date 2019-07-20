7b4be34800d2274234ac7975dc28f854a992c9b0
Jecsan Blanco <jecsanb@outlook.com>
2019-07-20 13:21:57 -0500

    Change configuration to inheritance rather than composition to make things easier.

--- src/main/java/com/usrmngr/client/core/model/Connectors/Configuration.java
+++ src/main/java/com/usrmngr/client/core/model/Connectors/Configuration.java
@@ -10,57 +10,36 @@ import java.util.Properties;
  * A generic configuration class that can hold different types of values.
  * The class is able to open files and write files to the desired paths.
  */
-public class Configuration {
-    private Properties values;
+public class Configuration extends  Properties{
     public Configuration() {
-        values = new Properties();
+        super();
     }
-
-    /**
-     * Deep Copy constructor
-     * @param conf another configuration
-     */
-    Configuration(Configuration conf) {
-        this.values = new Properties(conf.values);
-    }
-
-    /**
-     * Deep clone
-     * @param configuration
-     * @return deep clone of provided config
-     */
-    public Configuration clone(Configuration configuration) {
-        return new Configuration(configuration);
-    }
-    protected void put(String key, String value) {
-        values.put(key, value);
-    }
-
+     void  put(String key,String value){
+        super.put(key,value);
+     }
      String get(String key) {
-        return values.getProperty(key);
+        return super.getProperty(key);
     }
 
     /**
      * Attempts to load the configuration file at the path specified.
      * @throws IOException
      */
-    public void load(File file) throws IOException {
-        values.load(new FileInputStream(file));
+    public void load(String  path) throws IOException {
+        File file = new File(path);
+        this.load(new FileInputStream(file));
     }
 
     /**
      *  Attempts to save the configuration file at the path specified.
      * @throws IOException
      */
-    public void save(File file) throws IOException {
+    public void save(String path) throws IOException {
+        File file = new File(path);
         File parent = file.getParentFile();
-        if(parent != null) // create directory structure as needed
-            parent.mkdir();
-        file.createNewFile();
-        values.store(new FileOutputStream(file), "config");
+        if(parent != null && !parent.exists()) parent.mkdir();
+        super.store(new FileOutputStream(file), "config");
     }
-
-
     /**
      * Returns string representation of the config
      *
@@ -69,14 +48,15 @@ public class Configuration {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        values.forEach((key, value) -> sb.append(key).append("=").append(value).append("\n"));
+        this.forEach((key, value) -> sb.append(key).append("=").append(value).append("\n"));
         sb.deleteCharAt(sb.lastIndexOf("\n"));
         return sb.toString();
     }
 
-    public void clear() {
-        values.clear();
+    public static void main(String[] args) throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.put("Test","test");
+        configuration.save("c:\\\\temp\\config.txt");
     }
-    public static void main(String[] args) {
-    }
+
 }
